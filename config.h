86f55d67ab77966611adabb937372173d157ae6e
krolxon <krolyxon@tutanota.com>
2025-08-03 11:40:47 +0530

    fix compilation

--- config.h
+++ config.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-        {"",	"sb-battery",	5,	3},
 	{"",	"sb-music",	0,	11},
+  {"",	"sb-battery",	5,	3},
 	{"",	"sb-volume",	10,	10},
 	// {"",	"sb-updates",	360,	12},
 
 	 {"",	"sb-memory",	5,	14},
 	// {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-         {"",   "sb-clock",     60,      1},
+  {"",   "sb-clock",     60,      1},
 
 
 };
